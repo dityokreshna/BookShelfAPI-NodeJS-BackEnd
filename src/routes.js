2d3b4f685d213fa968aff0317e8f320f3a8fd463
Dityo Kreshna <dityo.kreshna354@gmail.com>
2022-07-19 17:01:46 +0700

    UploadV1
    Upload Document

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+const {
+  addBooksHandler,
+  getAllBooksHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
